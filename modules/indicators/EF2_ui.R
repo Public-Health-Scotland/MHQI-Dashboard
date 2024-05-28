ab35b55aa5034f0f385ebb355369cd4ddd7d8404
Cormac-Murray <cormac.murray@phs.scot>
2024-05-28 15:38:13 +0100

    EF2 updated

--- modules/indicators/EF2_ui.R
+++ modules/indicators/EF2_ui.R
@@ -1,9 +1,25 @@
 tabItem(tabName = EF2_tab,
         fluidPage(
-          titlePanel("EF2"),
+          titlePanel("EF2 - Readmissions to hospital within 28 days of discharge"),
           mainPanel(
-            h3("You are on EF2"),
-            fluidRow(
+            img(src='infographics/EF2.png',
+                class = "infographic",
+                alt = paste0(
+                  "The percentage of emergency readmissions within 28 days in ",
+                  "Scotland was 7.3% for the year-long period ending 31 March ",
+                  "2023. This compares with 7.8% for the year ending 31 March ",
+                  "2022 and 9.3% for the year ending 31 March 2021.")),
+            br(),
+            p("The data for EF1 is sourced from ",
+              a(href="https://www.nssdiscovery.scot.nhs.uk/",
+                "Discovery"),
+              " using SMR04 data.",
+              a(href="https://publichealthscotland.scot/services/data-management/data-management-in-secondary-care-hospital-activity/scottish-morbidity-records-smr/completeness/",
+                "Data completeness"),
+              " for SMR04 was around 90% or better when the data was accessed.")
+          ),
+          
+          fluidRow(
               column(4, actionButton(inputId = "EF2_scot_hub_button", 
                                      label = "Scotland Hub", icon = icon("home"),
                                      class = "navpageButton")),
@@ -14,6 +30,6 @@ tabItem(tabName = EF2_tab,
                                      label = "Next Page", icon = icon("arrow-right"),
                                      class = "navpageButton"))
             )
-          )
-        )
+          ) # End of fluidPage
+        
 )
