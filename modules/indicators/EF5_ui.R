9027283eb07a09074ecea395bdd70150e00178f1
Cormac-Murray <cormac.murray@phs.scot>
2024-05-28 16:06:30 +0100

    Ef5 updated.

--- modules/indicators/EF5_ui.R
+++ modules/indicators/EF5_ui.R
@@ -1,5 +1,19 @@
 tabItem(tabName = EF5_tab,
         fluidPage(
+          titlePanel("EF5 - % of did not attend appointments for community based services of people with mental health problems"),
+          mainPanel(
+            img(src='infographics/EF5.png',
+                class = "infographic",
+                alt = paste0(
+                  "The percentage of people who did not attend their ",
+                  "appointment for mental health problems at community based ",
+                  "services ranged from 8% to 19% for the quarter ending 30 ",
+                  "June 2023.")),
+            br(),
+            p("The data for EF5 is sourced is sourced from health board returns, which may be incomplete.")
+          ),
+          
+          fluidPage(
           titlePanel("EF5"),
           mainPanel(
             h3("You are on EF5"),
@@ -16,4 +30,5 @@ tabItem(tabName = EF5_tab,
             )
           )
         )
+)   # End of fluidPage
 )
