f9d1a111624e6962c50a52371d63797ed23b9abf
alex-bruce <lex.bruce.code@outlook.com>
2024-06-19 18:39:20 +0100

    Added data prep file

---
+++ data_preparation.R
@@ -0,0 +1,11 @@
+## Data preparation script
+# N.B. do not use any direct filepaths, ony relational ones
+
+## [Indicators] ----
+# E1 ----
+E1_data <- read.csv("data/E1.csv") %>% 
+  rename(dd_bed_days = delayed_discharge_bed_days,
+         fyear = financial_year)
+
+unique_area_types <- E1_data %>% 
+  distinct(area_type) %>% pull(area_type)
