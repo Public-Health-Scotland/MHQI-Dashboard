9161aaad551ade8c1298a16b4968fd04396d07be
alex-bruce <lex.bruce.code@outlook.com>
2024-07-12 17:33:10 +0100

    Minor grammar fix in intro

--- modules/introduction_ui.R
+++ modules/introduction_ui.R
@@ -2,7 +2,7 @@ tabItem(tabName = "intro",
         fluidPage(
           # titlePanel("Mental Health Quality Indicators Joint Collection Materials"),
           mainPanel(
-            h1("Welcome, to the Mental Health Quality Indicators Dashboard"),
+            h1("Welcome to the Mental Health Quality Indicators Dashboard"),
             p("This dashbaord covers 19 indicators out of the 32 indicators set 
               out in the "),
             p("3 of the indicators have been developed into interactive formats 
