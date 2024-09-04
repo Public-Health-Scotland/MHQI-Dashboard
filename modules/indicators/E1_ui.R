76a320d35e122e7b9b3d186a2db3af17521b7366
alex-bruce <lex.bruce.code@outlook.com>
2024-09-04 19:00:04 +0100

    Updated E1 tab to use boxes

--- modules/indicators/E1_ui.R
+++ modules/indicators/E1_ui.R
@@ -1,65 +1,125 @@
+### Beginning of E1 tab ----
+
 tabItem(tabName = "E1_tab",
         fluidPage(
-          titlePanel(paste0(
-            "E1 - Number of days people spend in hospital when they are ",
-            "clinically ready to be discharged (per 1,000 population)")),
-          mainPanel(
-            tags$p("Below is a graph showing the changes over time."),
-            
-            fluidRow(
-              column(6,uiOutput("E1_plot1_areaType_output")),
-              column(6,uiOutput("E1_plot1_areaName_output"))
+          ## Title for E1 tab ----
+          h1(paste0(
+            "E1 - Delayed Discharges: Number of days people spend in hospital ",
+            "when they are clinically ready to be discharged (per 1,000 population)")
             ),
+          
+          ## First Graph ----
+          # Text above graph
+          fluidRow(
+            column(12,
+                   box(width = NULL,
+                       p(paste0(
+                         "Below is a graph showing the total number of days spent ",
+                         "in hospital when patients are ready to be discharged over time, ",
+                         "broken down by either council area of residence or health board of treatment. ",
+                         "Use the drop down menus to select which areas you wish to look at."))
+                       )
+                   )
+          ), # End of fluidRow
+          
+          # Drop down menus
+          fluidRow(
+            column(6,
+                   box(width = NULL,
+                       uiOutput("E1_plot1_areaType_output"))
+            ),
+            column(6,
+                   box(width = NULL,
+                       uiOutput("E1_plot1_areaName_output"))
+            ),
+          ), # End of fluidRow
+          
+          # Graph 1 output
+          fluidRow(
+            box(width = 12,
+                plotlyOutput("E1_plot1",
+                             width = "100%"))
+          ),
+          
+          br(),
+          hr(), # page break
+          br(),
+          
+          ## Second Graph ----
+          # Text above graph
+          fluidRow(
+            column(12,
+                   box(width = NULL,
+                       p(paste0(
+                     "Below is a graph showing the total number of days spent ",
+                     "in hospital when patients are ready to be discharged over time, ",
+                     "broken down by either council area of residence or health board of treatment. ",
+                     "Use the drop down menus to select which areas you wish to look at."))
+                     )
+                   )
+          ), # End of fluidRow
+          
+          # Year Drop down
+          fluidRow(
+            column(6,
+                  box(width = NULL,
+                      uiOutput("E1_plot2_year_output"))
+                  )
+              ),
+          # Graph 2 output
+          fluidRow(
+            box(width = 12,
+                plotlyOutput("E1_plot2",
+                             width = "100%"))
+          ),
+          
+          
+          # mainPanel(
+          #   tags$p("Below is a graph showing the changes over time."),
+          #   
+          #   fluidRow(
+          #     column(6,uiOutput("E1_plot1_areaType_output")),
+          #     column(6,uiOutput("E1_plot1_areaName_output"))
+          #   ),
+          # 
+          #   fluidRow(
+          #     plotlyOutput("E1_plot1"
+          #                #   ,
+          #                # width = "100%",
+          #                # height = "50%"
+          #                )
+          #   ),
+          #   br(),
+          #   # dataTableOutput("diagnoses_table"),
+          #   br(),
+          #   br(),
+          # 
+          #   
+          #   tags$p("Below is a graph showing a comparison between health boards and NHS Scotland for your selected financial year."),
+          #   # br(),
+          #   
+          #   fluidRow(
+          #      column(6,uiOutput("E1_plot2_year_output"))
+          #   ),
+          #   
+          #   fluidRow(
+          #      plotlyOutput("E1_plot2"
+          #                   #   ,
+          #                   # width = "100%",
+          #                   # height = "50%"
+          #      )
+          #   ),
+          #   br(),
+          #   tags$p("Source: Delayed discharges in NHS Scotland annual publication.", 
+          #          br(), 
+          #          "Data shown is for ages 18 years and above"),
+          #   # as copied from above -- dataTableOutput("diagnoses_table"),
+          #   br(),
+          #   br(),
+            
+          br(),
 
-            fluidRow(
-              plotlyOutput("E1_plot1"
-                         #   ,
-                         # width = "100%",
-                         # height = "50%"
-                         )
-            ),
-            br(),
-            # dataTableOutput("diagnoses_table"),
-            br(),
-            br(),
-
-            
-            tags$p("Below is a graph showing a comparison between health boards and NHS Scotland for your selected financial year."),
-            # br(),
-            
-            fluidRow(
-               column(6,uiOutput("E1_plot2_year_output"))
-            ),
-            
-            fluidRow(
-               plotlyOutput("E1_plot2"
-                            #   ,
-                            # width = "100%",
-                            # height = "50%"
-               )
-            ),
-            br(),
-            tags$p("Source: Delayed discharges in NHS Scotland annual publication.", 
-                   br(), 
-                   "Data shown is for ages 18 years and above"),
-            # as copied from above -- dataTableOutput("diagnoses_table"),
-            br(),
-            br(),
-            
-            go_2_top_bttn,
-
-            # tags$a(href = '#E1_top',
-            #        icon("circle-arrow-up",
-            #             lib = "glyphicon"),
-            #        "Back to top"),
-            br(),
-            br(),
-          # ), # End of mainPanel
-          
-          
-          
-          
-          
+          ## Navigation Buttons ----
           fluidRow(
             column(4, actionButton(inputId = "E1_scot_hub_button", 
                                    label = "Scotland Hub", icon = icon("home"),
@@ -70,7 +130,8 @@ tabItem(tabName = "E1_tab",
             column(4, actionButton(inputId = "EF1_nextButton", 
                                    label = "Next Page", icon = icon("arrow-right"),
                                    class = "navpageButton"))
-          )
+          ),
+          # Insert go to top button
+          go_2_top_bttn
          )
       )
-)
