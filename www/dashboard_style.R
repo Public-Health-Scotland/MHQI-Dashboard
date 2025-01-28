c02d5e3dc16349d636fb697eb67b62e02530e39b
alex-bruce <lex.bruce.code@outlook.com>
2025-01-28 17:03:42 +0000

    Added descriptive commentary to dashboard style script

--- www/dashboard_style.R
+++ www/dashboard_style.R
@@ -1,16 +1,20 @@
+## This script defines some of the main colours of the dashboard 
+## by creating a theme
+
 # Define theme for shinydashboard elements
 use_theme(create_theme(
   
+  # Background colour for app pages
   adminlte_global(
     content_bg = "#ECEBF3"
   ),
   
-  # Sidebar colours
+  # Sidebar colours ----
   adminlte_sidebar(
-    dark_bg = "#3F3685",
-    dark_color = "#ECEBF3",
-    dark_hover_bg = "#9B4393",
-    dark_hover_color = "#ECEBF3"
+    dark_bg = "#3F3685", # background colour
+    dark_color = "#ECEBF3", # text colour
+    dark_hover_bg = "#9B4393", # background hover colour
+    dark_hover_color = "#ECEBF3" # text hover colour
   )
 )
 ) # End of theme
