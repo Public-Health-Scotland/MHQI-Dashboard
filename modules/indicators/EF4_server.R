34b0f91fdecf364cd789fb2e377dce25d1305ddf
MahriScot <mahristewart@hotmail.co.uk>
2025-03-05 12:26:36 +0000

    EF4 updates for April 2025 release - latest data added to graphs and tables and links to publication updated.

--- modules/indicators/EF4_server.R
+++ modules/indicators/EF4_server.R
@@ -4,7 +4,7 @@
 output$EF4_trendPlot_hbName_output <- renderUI({
   shinyWidgets::pickerInput(
     "EF4_trendPlot_hbName",
-    label = "Select NHS Health Board:",
+    label = "Select NHS health board:",
     choices = EF4_hb_names,
     selected = "NHS Scotland")
 })
@@ -42,7 +42,7 @@ EF4_trendPlot_data <- reactive({
        ggplot(data = EF4_trendPlot_data(),
               aes(x = fyear,
                   y = value,
-                  text = paste0("Financial year: ",         # for tooltip in ggplotly - shows values on hover
+                  text = paste0("Financial Year: ",         # for tooltip in ggplotly - shows values on hover
                                 EF4_trendPlot_data()$fyear,
                                 "<br>",
                                 "Health Board: ",
@@ -137,141 +137,3 @@ EF4_trendPlot_data <- reactive({
   )
   
   
-
-# X OLD CODE ----
-# 
-# ## plotly bar chart ----
-# output$EF4_trendPlot <- renderPlotly({
-#   #
-#   #   # # No data plot
-#   #   # if(sum(EF4_trendPlot_data()$value) == 0 &
-#   #   #    !is.na(sum(EF4_trendPlot_data()$value)))
-#   #   #
-#   #   # {
-#   #   #   noDataPlot(phs_colours("phs-purple"))
-#   #   # }
-#   #   #
-#   #   # else {
-#   #
-#   ### Tooltip creation ----
-#   tooltip_EF4 <- paste0("Financial year: ",
-#                        EF4_trendPlot_data()$fyear,
-#                        "<br>",
-#                        "Health Board: ",
-#                        EF4_trendPlot_data()$hb_name,
-#                        "<br>",
-#                        EF4_trendPlot_data()$measure,": ",
-#                        EF4_trendPlot_data()$value)
-# 
-#   ## Create the main body of the chart ----
-# 
-#   # phs_scatterPlot(EF4_trendPlot_data, fyear, value, tooltip_EF4, measure)
-#   
-#   plot_ly(data = EF4_trendPlot_data(),
-#           # Select your variables.
-#           x = ~fyear, y = ~value, color = ~measure,
-#           text = tooltip_EF4, hoverinfo = "text",
-#           colors = c("#0078D4", "#3393DD", "#80BCEA", "#B3D7F2"), # line colours
-#           type = 'scatter', mode = 'lines+markers',
-#           # width = 600, height = 300,
-#           line = list(width = 2),
-#           linetype = ~measure,
-#           linetypes = c("solid", "dash"),
-#           marker = list(size = 8),
-#           name = ~str_wrap(measure, 19)) %>% # legend labels
-#     
-#     # data = EF4_trendPlot_data(),
-#     #       # Select your variables.
-#     #       x = ~fyear, y = ~value,
-#     #       text = tooltip_EF4, hoverinfo = "text",
-#     #       color = ~measure,
-#     #       colors = c("#0078D4", "#3393DD", "#80BCEA", "#B3D7F2"), # line colours
-#     #       type = 'scatter', mode = 'lines+markers',
-#     #       # width = 600, height = 300,
-#     #       line = list(width = 2),
-#     #       linetype = ~measure,
-#     #       linetypes = c("solid", "dash"),
-#     #       marker = list(size = 8),
-#     #       name = ~str_wrap(measure, 19)) %>% # legend labels
-# 
-#   ### 7 - Graph title ----
-#   layout(title =
-#            paste0(
-#              "<b>",
-#              "Percent (%) of total NHS spend in ",
-#              input$EF4_trendPlot_hbName_output, ",",
-#              "<br>",
-#              first(as.vector(EF4_trendPlot_data()$fyear)),
-#              " to ",
-#              last(as.vector(EF4_trendPlot_data()$fyear)),
-#              "<br>",
-#              "</b>"
-#            ),
-# 
-#          separators = ".",
-# 
-#          yaxis = list(
-#            exponentformat = "none",
-#            separatethousands = TRUE,
-#            range = c(0, max(EF4_trendPlot_data()$value, na.rm = TRUE)
-#                      + (max(EF4_trendPlot_data()$value, na.rm = TRUE)*0.1)
-#            ),
-#            # Define Y axis title
-#            title = paste0(c(rep("&nbsp;", 20),
-#                             "Percent",
-#                             rep("&nbsp;", 20),
-#                             rep("\n&nbsp;", 3))),
-#            showline = TRUE,
-#            ticks = "outside"
-#          ),
-# 
-#          xaxis = list(
-#            title = paste0(c("<br>", "Financial year")),
-#            showline = TRUE,
-#            ticks = "outside"
-#          ),
-# 
-#          margin = list(l = 90, r = 60, b = 180, t = 120),
-#          title = list(size = 15),
-#          font = list(size = 13),
-#          # Legend
-#          showlegend = TRUE,
-#          legend = list(x = 1,
-#                        y = 1,
-#                        bgcolor = 'rgba(255, 255, 255, 0)',
-#                        bordercolor = 'rgba(255, 255, 255, 0)')
-#          # legend = list(orientation = "h",   # show entries horizontally
-#          #               xanchor = "center",  # use center of legend as anchor
-#          #               x = 0.5, y = -0.9)             # put legend in center of x-axis
-#   ) %>%
-# 
-#     ### Remove unnecessary buttons from the modebar ----
-# 
-#   config(displayModeBar = TRUE,
-#          modeBarButtonsToRemove = bttn_remove,
-#          displaylogo = F, editable = F)
-# 
-#   # }
-# 
-# })
-# 
-# 
-# ### Table below graph ----
-# 
-# # output$EF4_1_trend_table <- renderDataTable({
-# #   datatable(EF4_trendPlot_data(),
-# #             style = 'bootstrap',
-# #             class = 'table-bordered table-condensed',
-# #             rownames = FALSE,
-# #             options = list(pageLength = 16, autoWidth = TRUE, dom = 'tip'),
-# #             colnames = c("Financial year",
-# #                          "Area of Residence",
-# #                          "Number of Bed Days"))
-# # })
-# 
-# 
-# 
-
-
-
-
