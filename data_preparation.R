a169045f03de280b3fd33ddb54d7e4033d0ee6c4
alex-bruce <lex.bruce.code@outlook.com>
2025-03-10 17:47:45 +0000

    Factoring EF5 data to fix X axis

--- data_preparation.R
+++ data_preparation.R
@@ -49,7 +49,18 @@ EF4_trend_measures <- c('Mental Health Expenditure (%)', 'CAMHS Expenditure (%)'
 ## EF5 ----
 EF5_data <- read.csv("data/EF5.csv") %>% 
   # Below line is not needed, but keeping in as it shows the variable names clearly
-  select(hb_name, year_months, measure, value)
+  select(hb_name, year_months, measure, value) %>% 
+  # So that months aren't alphabetical:
+  mutate(year_months = fct_relevel(year_months, 
+                                   "Jan-Mar 2022", "Apr-Jun 2022", 
+                                   "Jul-Sep 2022", "Oct-Dec 2022", 
+                                   "Jan-Mar 2023", "Apr-Jun 2023", 
+                                   "Jul-Sep 2023", "Oct-Dec 2023",
+                                   "Jan-Mar 2024", "Apr-Jun 2024", 
+                                   "Jul-Sep 2024", "Oct-Dec 2024"#,
+                                   #"Jan-Mar 2025", "Apr-Jun 2025", 
+                                   #"Jul-Sep 2025", "Oct-Dec 2025"
+  ))
 
 # Isolating necessary values for selectors
 EF5_quarter <- EF5_data %>% 
