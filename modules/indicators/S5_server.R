1def6cfd174f4d3ba6003fbfff10d29d4802f181
MahriScot <mahristewart@hotmail.co.uk>
2025-04-08 16:10:32 +0100

    S5 graph 1 now only uses plotly (no ggplot), S5 graph 2 x-axis now fluid (still ggplot + plotly)

--- modules/indicators/S5_server.R
+++ modules/indicators/S5_server.R
@@ -1,17 +1,28 @@
 # S5 Trends Plot(s) ----
 
-# Graph 1 - comparing health boards over time ----
+# Two Graphs: 
+# 1. HB trend graph - user selects up to 4 HBs to compare incidents per 1000 bed days over time
+# 2. Measures graph for quarter - user selects calendar quarter to compare HBs incidents. 
 
-## Picker for user selecting specific health board ---- 
+# Tables: 
+# Graphs show incidents per 1000 bed days, but tables also include "total bed days" and 
+# "no. of incidents"
+
+
+# S2 - Graph 1 - comparing HBs over time ---- 
+
+## Health Board Selector ---- 
+# Picker for user selecting up to 4 health boards
 
 output$S5_trendPlot_hbName_output <- renderUI({
    shinyWidgets::pickerInput(
       "S5_trendPlot_hbName",
-      label = "Select NHS health board (Maximum 4):",
-      choices = sort(unique(as.character(S5_data$nhs_health_board))),
+      label = "Select NHS health board(s) (Maximum 4):",
+      choices = S5_hb_names,  # pulled out in data_preparation.R
       multiple = TRUE,
       options = list("max-options" = 4,
                      `selected-text-format` = "count > 1"),
+      # No Scotland data has been calculated for HB returns indicators: S2, S5, EF5
       selected = "NHS Ayrshire & Arran")
 })
 
@@ -30,79 +41,183 @@ S5_trendPlot_data <- reactive({
 ### Render plotly ----
 
 output$S5_trendPlot <- renderPlotly({ 
+  
+   # Plotly version of graph 
    
-   ### Create reactive ggplot graph ----
-   
-   S5_plot_graph <- reactive ({
+   plot_ly(data = S5_trendPlot_data(), 
+           
+           x = ~year_months, 
+           y = ~incidents_per_1000_bed_days, 
+           color = ~nhs_health_board, 
+           
+           # Tooltip text
+           text = paste0("Calendar quarter: ",                
+                         S5_trendPlot_data()$year_months, 
+                         "<br>",
+                         "Health board: ",
+                         S5_trendPlot_data()$nhs_health_board,
+                         "<br>",
+                         "Incidents per 1,000 bed days: ",
+                         S5_trendPlot_data()$incidents_per_1000_bed_days), 
+           hoverinfo = "text", 
+           
+           # Line aesthetics: 
+           type = 'scatter',
+           mode = 'lines+markers', 
+           line = list(width = 3), 
+           colors = c("#3F3685", "#9B4393", "#0078D4", "#1E7F84"),
+           linetype = ~nhs_health_board, 
+           linetypes = c("solid", "dashed", "solid", "dashed"),   # Note it is  "dot" rather than "dashed" in EF5
+           symbol = ~nhs_health_board,
+           symbols = c("circle", "square", "triangle-up", "triangle-down"),
+           marker = list(size = 12),
+           # Size of graph:
+           # width = 1000, 
+           height = 600,
+           # Legend info:
+           name = ~str_wrap(nhs_health_board, 15)) %>%
       
-      ggplot(data = S5_trendPlot_data(), 
-             aes(x = year_months, 
-                 y = incidents_per_1000_bed_days,  
-                 # for tooltip in ggplotly - shows values on hover:
-                 text = paste0("Calendar quarter: ",                
-                               S5_trendPlot_data()$year_months, 
-                               "<br>",
-                               "Health board: ",
-                               S5_trendPlot_data()$nhs_health_board,
-                               "<br>",
-                               "Incidents per 1,000 bed days: ",
-                               S5_trendPlot_data()$incidents_per_1000_bed_days))) + 
-         geom_line() +
-         geom_point(size = 2.5) +
-         aes(group = nhs_health_board,
-             linetype = nhs_health_board,
-             color = nhs_health_board,   # Have to do this outside so that the legends shows and so that there aren't 3 legends
-             shape = nhs_health_board) +
-         scale_color_discrete_phs(name = "Health board", 
-                                  palette = "main-blues",
-                                  labels = ~ stringr::str_wrap(.x, width = 15)) +
-         # scale_color_manual(name = "Area name", 
-         #                    values = c("#0078D4", "#3393DD", "#80BCEA", "#B3D7F2"),
-         #                    labels = ~ stringr::str_wrap(.x, width = 15)) +
-         scale_linetype_manual(name = "Health board", 
-                               values = c("solid", "dashed", "solid", "dashed"),
-                               labels = ~ stringr::str_wrap(.x, width = 15)) +
-         scale_shape_manual(name = "Health board", 
-                            values = c("circle", "square", "triangle-up", "triangle-down"), 
-                            labels = ~ stringr::str_wrap(.x, width = 15)) +
-         theme_classic() +                            # de-clutters graph background
-         theme(panel.grid.major.x = element_line(),  # Shows vertical grid lines 
-               panel.grid.major.y = element_line(),  # Shows horizontal grid lines 
-               axis.title.x = element_text(size = 12,
-                                           color = "black",
-                                           face = "bold"),
-               axis.text.x = element_text(angle = 25),
-               axis.title.y = element_text(size = 12,
-                                           color = "black",
-                                           face = "bold"),
-               legend.text = element_text(size = 8, 
-                                          colour = "black"), 
-               legend.title = element_text(size = 9, 
-                                           colour = "black", 
-                                           face = "bold")) +  
-         labs(x = "\n Calendar Quarter", 
-              y = "Incidents per 1,000 occupied bed days") +
-         scale_y_continuous(expand = c(0, 0),   # Ensures y axis starts from zero (important as some are zero or NA)
-                            limits = c(0, 100)  # NOTE THAT THIS NEEDS TO BE FLUID - TRYING BELOW - as some hb's quarters are near 100 (Western Isles
-                            # Below is not yet working, probably due to NAs?:
-                            # limits = c(0, (max(S5_data$incidents_per_1000_bed_days) + 9)), # Keeps the y-axis the same length for all graphs, ranges from 0 to the max value plus 9 so that the last tick can be seen
-                            # breaks = seq(0, (max(S5_data$incidents_per_1000_bed_days) + 9), by = 10))   # y-axis ticks range from 0 to the max value + 9, showing increments of 10
-         )
-   })
-   
-
-   ### Run ggplot graph through plotly ----
-   
-   ggplotly(S5_plot_graph(),
-            # uses text set up in ggplot aes above:
-            tooltip = "text") %>%         
-      ### Remove unnecessary buttons from the modebar ----
-   config(displayModeBar = TRUE,
-          modeBarButtonsToRemove = bttn_remove,
-          displaylogo = F, editable = F)
-   
+      layout(title = str_wrap(paste0("<b>", 
+                                     "Incidents of violence per 1,000 bed days in selected health board(s), ", 
+                                     # first(S5_trendPlot_data()$year_months)," to ", last(S5_trendPlot_data()$year_months),
+                                     " by calendar quarter.", 
+                                     "</b>"), 
+                              54), 
+             yaxis = list(exponentformat = "none",
+                          separatethousands = TRUE,  # it's per 1,000 so do we need to do this? 
+                          range = c(0, max(S5_trendPlot_data()$incidents_per_1000_bed_days, na.rm = TRUE) * 110 / 100), # May have to edit this, had to change na.rm = TRUE to false
+                        
+                        
+                         # Wrap the y axis title in spaces so it doesn't cover the...
+                         # tick labels.
+                         title = #list(font = list(size = 13),
+                            paste0(c(rep("&nbsp;", 20),
+                                     print("Incidents per 1,000 occupied bed days"), 
+                                     rep("&nbsp;", 20),
+                                     rep("\n&nbsp;", 3)),
+                                     collapse = ""),#),
+                         showline = TRUE, 
+                         ticks = "outside"
+             ),
+             
+             # Create diagonal x-axis ticks
+             xaxis = list(tickangle = -45, 
+                          title = paste0(c(rep("&nbsp;", 20),
+                                           "<br>",
+                                           "<br>",
+                                           "Calendar quarter",
+                                           rep("&nbsp;", 20),
+                                           rep("\n&nbsp;", 3)),
+                                         collapse = ""),
+                          showline = TRUE, 
+                          ticks = "outside"),
+             
+             # Set the graph margins.
+             margin = list(l = 90, r = 60, b = 170, t = 90),  
+             
+             # Set the font sizes.
+             font = list(size = 13),
+             
+             # Add a legend so that the user knows which colour, line type...
+             # and symbol corresponds to which location of treatment.
+             # Make the legend background and legend border white.              
+             showlegend = TRUE,
+             legend = list(x = 1, 
+                           y = 0.8, 
+                           bgcolor = 'rgba(255, 255, 255, 0)', 
+                           bordercolor = 'rgba(255, 255, 255, 0)')) %>%
+      
+      # Remove any buttons we don't need from the modebar.
+      config(displayModeBar = TRUE,
+             modeBarButtonsToRemove = list('select2d', 'lasso2d', 
+                                           # 'zoomIn2d', 'zoomOut2d', 'autoScale2d', 
+                                           'toggleSpikelines', 
+                                           'hoverCompareCartesian', 
+                                           'hoverClosestCartesian'), 
+             displaylogo = F, 
+             editable = F)
+    
    
 })
+
+
+      ## OLD GGplot2 version of graph run through plotly ----
+      ### Render plotly
+
+      # output$S5_trendPlot <- renderPlotly({ 
+      #    
+      #    ### Create reactive ggplot graph
+      #    
+      #    S5_plot_graph <- reactive ({
+      #       
+      #       ggplot(data = S5_trendPlot_data(), 
+      #              aes(x = year_months, 
+      #                  y = incidents_per_1000_bed_days,  
+      #                  # for tooltip in ggplotly - shows values on hover:
+      #                  text = paste0("Calendar quarter: ",                
+      #                                S5_trendPlot_data()$year_months, 
+      #                                "<br>",
+      #                                "Health board: ",
+      #                                S5_trendPlot_data()$nhs_health_board,
+      #                                "<br>",
+      #                                "Incidents per 1,000 bed days: ",
+      #                                S5_trendPlot_data()$incidents_per_1000_bed_days))) + 
+      #          geom_line() +
+      #          geom_point(size = 2.5) +
+      #          aes(group = nhs_health_board,
+      #              linetype = nhs_health_board,
+      #              color = nhs_health_board,   # Have to do this outside so that the legends shows and so that there aren't 3 legends
+      #              shape = nhs_health_board) +
+      #          scale_color_discrete_phs(name = "Health board", 
+      #                                   palette = "main-blues",
+      #                                   labels = ~ stringr::str_wrap(.x, width = 15)) +
+      #          # scale_color_manual(name = "Area name", 
+      #          #                    values = c("#0078D4", "#3393DD", "#80BCEA", "#B3D7F2"),
+      #          #                    labels = ~ stringr::str_wrap(.x, width = 15)) +
+      #          scale_linetype_manual(name = "Health board", 
+      #                                values = c("solid", "dashed", "solid", "dashed"),
+      #                                labels = ~ stringr::str_wrap(.x, width = 15)) +
+      #          scale_shape_manual(name = "Health board", 
+      #                             values = c("circle", "square", "triangle-up", "triangle-down"), 
+      #                             labels = ~ stringr::str_wrap(.x, width = 15)) +
+      #          theme_classic() +                            # de-clutters graph background
+      #          theme(panel.grid.major.x = element_line(),  # Shows vertical grid lines 
+      #                panel.grid.major.y = element_line(),  # Shows horizontal grid lines 
+      #                axis.title.x = element_text(size = 12,
+      #                                            color = "black",
+      #                                            face = "bold"),
+      #                axis.text.x = element_text(angle = 25),
+      #                axis.title.y = element_text(size = 12,
+      #                                            color = "black",
+      #                                            face = "bold"),
+      #                legend.text = element_text(size = 8, 
+      #                                           colour = "black"), 
+      #                legend.title = element_text(size = 9, 
+      #                                            colour = "black", 
+      #                                            face = "bold")) +  
+      #          labs(x = "\n Calendar Quarter", 
+      #               y = "Incidents per 1,000 occupied bed days") +
+      #          scale_y_continuous(expand = c(0, 0),   # Ensures y axis starts from zero (important as some are zero or NA)
+      #                             limits = c(0, 100)  # NOTE THAT THIS NEEDS TO BE FLUID - TRYING BELOW - as some hb's quarters are near 100 (Western Isles
+      #                             # Below is not yet working, probably due to NAs?:
+      #                             # limits = c(0, (max(S5_data$incidents_per_1000_bed_days) + 9)), # Keeps the y-axis the same length for all graphs, ranges from 0 to the max value plus 9 so that the last tick can be seen
+      #                             # breaks = seq(0, (max(S5_data$incidents_per_1000_bed_days) + 9), by = 10))   # y-axis ticks range from 0 to the max value + 9, showing increments of 10
+      #          )
+      #    })
+      #    
+      # 
+      #    ### Run ggplot graph through plotly
+      #    
+      #    ggplotly(S5_plot_graph(),
+      #             # uses text set up in ggplot aes above:
+      #             tooltip = "text") %>%         
+      #       ### Remove unnecessary buttons from the modebar
+      #    config(displayModeBar = TRUE,
+      #           modeBarButtonsToRemove = bttn_remove,
+      #           displaylogo = F, editable = F)
+      #    
+      #    
+      # })
 
 
 
@@ -149,7 +264,7 @@ output$S5_plot2_quarter_output <- renderUI({
       "S5_plot2_quarter",
       label = "Select calendar quarter:",
       choices = unique(S5_data$year_months),
-      selected = "Jul-Sep 2024")
+      selected = "Oct-Dec 2024")
 })
 
 ## Selecting appropriate data for graph 2 ---- 
@@ -205,11 +320,11 @@ output$S5_plot2 <- renderPlotly({
          
          # Not yet all working
          scale_x_continuous(na.value = 0,  # required for adding "NA" value to graph
-                            # Below not yet working. Because of NAs? 
-                            # limits = c(0, (max(S5_data$incidents_per_1000_bed_days) + 9)),# Keeps the x-axis the same length for all graphs, ranges from 0 to the max value plus 9 so that the last tick can be seen
-                            # breaks = seq(0, (max(S5_data$incidents_per_1000_bed_days) + 9), by = 10),  # x-axis ticks range from 0 to the max value + 9, showing increments of 10
-                            limits = c(0, 100),  # NOTE THAT THIS NEEDS TO BE FLUID ^ TRYING ABOVE as some hb's quarters are near 100 (Western Isles)
-                            n.breaks = 10,
+                            # Trying to make limits fluid but breaks error = requires a finite number in the "to=" (is this a problem if there are NAs?):
+                            limits = c(0, max(S5_data$incidents_per_1000_bed_days) + 10), # x-axis now fluid, ranges from 0 to the max value plus 10 so that the last tick can be seen (this part is not working)
+                           # breaks = seq(0, (max(S5_data$incidents_per_1000_bed_days) + 10), by = 5),  # x-axis ticks range from 0 to the max value + 10, in increments of 5
+                            breaks = seq(0, 150, by = 5),  # x-axis ticks range from 0 to 150 (chosen as current max is <100, will be an issue if any value is >150 in future), showing increments of 5 as most values are < 40
+                            # n.breaks = 11,   # This didn't change anything
                             expand = c(0, 0))  # remove spacing between hb names on y axis and 0% on x axis
       })
                             
