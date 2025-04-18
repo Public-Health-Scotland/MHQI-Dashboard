e28a99c35ee7ee351f7687a51144641e496ca231
Mahri <mahri.stewart@phs.scot>
2025-04-18 15:11:04 +0000

    S2 - graph 3 to plotly - ongoing issue with xaxis not showing all quarters from Jan 2022 if there is no data for that quarter though. It only starts from the Quarter that has data.

--- modules/indicators/S2_server.R
+++ modules/indicators/S2_server.R
@@ -338,125 +338,223 @@ output$S2_2_table_download <- downloadHandler(
 
 # Graph 3 - by HB - Total discharged and number followed up ---- 
 
-## Picker 1 - for user selecting Health Board  ----
-
-output$S2_Plot3_hbName_output <- renderUI({
-   shinyWidgets::pickerInput(
-      "S2_Plot3_hbName",
-      label = "Select health board:",
-      choices = sort(unique(as.character(S2_data$nhs_health_board))),
-      selected = "NHS Ayrshire & Arran"
-   )
-})
-
-## Selecting appropriate data for graph and table 3 ----
-#Need to do both as we need Numeric columns pivoted in graph and Character columns not pivoted in table.
-
-S2_plot3_data_for_graph <- reactive({
-   S2_pivoted_data %>%
-      filter(nhs_health_board %in% input$S2_Plot3_hbName) 
-})
-
-
-S2_plot3_data_for_table <- reactive({
-   S2_data %>%
-      filter(nhs_health_board %in% input$S2_Plot3_hbName) %>%
-      select(!c("year_quarter", "year"))
-})
-
-## Create the line graph ----
-
-### Render plotly ----
-
-output$S2_plot3 <- renderPlotly({
+   ## Picker 1 - for user selecting Health Board  ----
    
-   S2_plot3_graph <- reactive({
-      ggplot(S2_plot3_data_for_graph(),
-             aes(x = year_months,
-                 y = number,
-                 text = paste0("Calendar quarter: ", S2_plot3_data_for_graph()$year_months,        # for tooltip in ggplotly - shows values on hover
+   output$S2_Plot3_hbName_output <- renderUI({
+      shinyWidgets::pickerInput(
+         "S2_Plot3_hbName",
+         label = "Select health board:",
+         choices = sort(unique(as.character(S2_data$nhs_health_board))),
+         selected = "NHS Ayrshire & Arran"
+      )
+   })
+
+   ## Selecting appropriate data for graph and table 3 ----
+   #Need to do both as we need Numeric columns pivoted in graph and Character columns not pivoted in table.
+   
+   S2_plot3_data_for_graph <- reactive({
+      S2_pivoted_data %>%
+         filter(nhs_health_board %in% input$S2_Plot3_hbName) 
+   })
+  
+   
+   S2_plot3_data_for_table <- reactive({
+      S2_data %>%
+         filter(nhs_health_board %in% input$S2_Plot3_hbName) %>%
+         select(!c("year_quarter", "year"))
+   })
+
+   ## Create graph 3 - line graph ----
+
+      ### Render plotly ----
+      
+      output$S2_plot3 <- renderPlotly({
+         
+         plot_ly(data = S2_plot3_data_for_graph(),
+                 
+                 x = ~year_months, 
+                 y = ~number, 
+                 color = ~total_or_followed_up, 
+                 
+                 # Tooltip text - No followed up OR Total discharged
+                 text = paste0("Calendar quarter: ", S2_plot3_data_for_graph()$year_months,        
                                "<br>",
                                "NHS health board: ", S2_plot3_data_for_graph()$nhs_health_board,
                                "<br>",
-
-                               case_when(total_or_followed_up == "Number of patients followed up" ~
+                               case_when(S2_plot3_data_for_graph()$total_or_followed_up == "Number of patients followed up" ~
                                             paste0("Number of patients followed up: ", S2_plot3_data_for_graph()$number),
-                                         total_or_followed_up == "Total number of discharged inpatients" ~
+                                         S2_plot3_data_for_graph()$total_or_followed_up == "Total number of discharged inpatients" ~
                                             paste0("Total number of inpatients discharged: ", S2_plot3_data_for_graph()$number),
-                                         TRUE ~ "No Data")))) +
-         geom_line() + 
-         geom_point(size = 2.5) + 
-         aes(group = total_or_followed_up,  # Have to do this outside so that the legends shows and so that there aren't 3 legends
-             linetype = total_or_followed_up, 
-             color = total_or_followed_up, 
-             shape = total_or_followed_up) + 
-         scale_color_discrete_phs(name = unique(S2_plot3_data_for_graph()$nhs_health_board), 
-                                  palette = "main-blues",
-                                  labels = ~ stringr::str_wrap(.x, width = 15)) +
-         scale_linetype_manual(name = unique(S2_plot3_data_for_graph()$nhs_health_board),
-                               values = c("solid", "dashed", "solid", "dashed"),
-                               labels = ~ stringr::str_wrap(.x, width = 15)) +
-         scale_shape_manual(name = unique(S2_plot3_data_for_graph()$nhs_health_board), 
-                            values = c("circle", "square", "triangle-up", "triangle-down"),
-                            labels = ~ stringr::str_wrap(.x, width = 15)) +
-          # geom_text(aes(label = if_else(is.na(number),
-          #                               "NA", "")), # "value" shown on graph is "NA" for NAs, no text for HBs with values
-          #           na.rm = FALSE,
-          #           nudge_y = 2,
-          #           size = 4) +
-         theme_classic() +
-         theme(panel.grid.major.x = element_line(),
-               panel.grid.major.y = element_line(),
-               axis.title.x = element_text(size = 12,
-                                           color = "black",
-                                           face = "bold"),
-               axis.text.x = element_text(angle = 25)) + 
-         labs(x = paste0("Calendar Quarter"),
-              y = "Number of Patients") +
-         scale_y_continuous(#na.value = 0,   # for placing "NA" but this isn't working nicely (brings graph line down to wherever you place it)
-                            n.breaks = 10,
-                            expand = c(0,0)) +  # remove spacing between dates on x axis and 0  on y axis
-         coord_cartesian(ylim = c(0, (max(S2_plot3_data_for_graph()$number) + 
-                                         (0.1*max(S2_plot3_data_for_graph()$number)))), 
-                         expand = TRUE)
-        # ylim = c(0, (max(S2_plot3_data_for_graph()$number) + (0.1*max(S2_plot3_data_for_graph()$number)))) # having issues with NAs not allowing axis to extend 
+                                         TRUE ~ "No Data")),
+                 hoverinfo = "text",
+                 
+                 # Line aesthetics: 
+                 type = 'scatter',
+                 mode = 'lines+markers', 
+                 line = list(width = 3), 
+                 colors = c("#3F3685", "#1E7F84"),
+                 linetype = ~total_or_followed_up, 
+                 linetypes = c("solid", "dashed"), 
+                 symbol = ~total_or_followed_up,
+                 symbols = c("circle", "triangle-up"),
+                 marker = list(size = 12),
+                 # Size of graph:
+                 height = 600,
+                 # Legend info:
+                 name = ~str_wrap(total_or_followed_up, 15)) %>%
+            
+            layout(yaxis = list(exponentformat = "none",
+                                separatethousands = TRUE,  
+                                range = c(0, max(S2_plot3_data_for_graph()$number) * 110 / 100), 
+                                # Wrap the y axis title in spaces so it doesn't cover the tick labels:
+                                title = paste0(c(rep("&nbsp;", 20),
+                                                 print("Number of Patients"), 
+                                                 rep("&nbsp;", 20),
+                                                 rep("\n&nbsp;", 3)),
+                                               collapse = ""),#),
+                                showline = TRUE, 
+                                ticks = "outside"
+                                ),
+                   
+                   xaxis = list(# X axis won't show quarters until there is one with data in it
+                      # e.g. NHS D&G only have data from Jan 2023 so the axis starts there 
+                      # instead of Jan 2022. The table is correct and showing NAs, but the 
+                      # below isn't working. 
+                      # type = "category",   # Ensures months aren't removed if there is no data
+                      #  tickmode = "array",         # Ensure all months are ticked
+                      #  tickvals = S2_plot3_data_for_graph()$year_months, # Explicitly set tick values to include all months
+                      #  ticktext = S2_plot3_data_for_graph()$year_months,  # Set the text of ticks to be the month names
+                      type = "category",   # meant to ensure that quarters aren't removed if there is no data
+                      tickmode = "array", S2_plot3_data_for_graph()$year_months, # meant to ensure that all quarters are ticked
+                      tickvals = levels(S2_plot3_data_for_graph()$year_months),  # Explicitly setting the tickvals to all factor levels
+                      ticktext = levels(S2_plot3_data_for_graph()$year_months),   # Ensuring that all levels appear as labels       
+                      tickangle = -45,    # Diagonal x-axis ticks
+                                title = paste0(c(rep("&nbsp;", 20),
+                                                 "<br>",
+                                                 "<br>",
+                                                 "Calendar Quarter",
+                                                 rep("&nbsp;", 20),
+                                                 rep("\n&nbsp;", 3)),
+                                               collapse = ""),
+                                showline = TRUE, 
+                                ticks = "outside"),
+                     
+                   
+                   # Set the graph margins:
+                   margin = list(l = 90, r = 60, b = 170, t = 90),  
+                   
+                   # Set the font sizes:
+                   font = list(size = 13),
+                   
+                   # Add a legend & make the legend background and border white:            
+                   showlegend = TRUE,
+                   legend = list(x = 1, 
+                                 y = 0.8, 
+                                 bgcolor = 'rgba(255, 255, 255, 0)', 
+                                 bordercolor = 'rgba(255, 255, 255, 0)')) %>%
+            
+            # Remove any buttons we don't need from the modebar.
+            config(displayModeBar = TRUE,
+                   modeBarButtonsToRemove = list('select2d', 'lasso2d', 
+                                                 # 'zoomIn2d', 'zoomOut2d', 'autoScale2d', 
+                                                 'toggleSpikelines', 
+                                                 'hoverCompareCartesian', 
+                                                 'hoverClosestCartesian'), 
+                   displaylogo = F, 
+                   editable = F)
+         
+      })
+         
+    
+   ### Old ggplot code ----      
+      #    
+      #    S2_plot3_graph <- reactive({
+      #       ggplot(S2_plot3_data_for_graph(),
+      #              aes(x = year_months,
+      #                  y = number,
+      #                  text = paste0("Calendar quarter: ", S2_plot3_data_for_graph()$year_months,        # for tooltip in ggplotly - shows values on hover
+      #                                "<br>",
+      #                                "NHS health board: ", S2_plot3_data_for_graph()$nhs_health_board,
+      #                                "<br>",
+      # 
+      #                                case_when(total_or_followed_up == "Number of patients followed up" ~
+      #                                             paste0("Number of patients followed up: ", S2_plot3_data_for_graph()$number),
+      #                                          total_or_followed_up == "Total number of discharged inpatients" ~
+      #                                             paste0("Total number of inpatients discharged: ", S2_plot3_data_for_graph()$number),
+      #                                          TRUE ~ "No Data")))) +
+      #          geom_line() + 
+      #          geom_point(size = 2.5) + 
+      #          aes(group = total_or_followed_up,  # Have to do this outside so that the legends shows and so that there aren't 3 legends
+      #              linetype = total_or_followed_up, 
+      #              color = total_or_followed_up, 
+      #              shape = total_or_followed_up) + 
+      #          scale_color_discrete_phs(name = unique(S2_plot3_data_for_graph()$nhs_health_board), 
+      #                                   palette = "main-blues",
+      #                                   labels = ~ stringr::str_wrap(.x, width = 15)) +
+      #          scale_linetype_manual(name = unique(S2_plot3_data_for_graph()$nhs_health_board),
+      #                                values = c("solid", "dashed", "solid", "dashed"),
+      #                                labels = ~ stringr::str_wrap(.x, width = 15)) +
+      #          scale_shape_manual(name = unique(S2_plot3_data_for_graph()$nhs_health_board), 
+      #                             values = c("circle", "square", "triangle-up", "triangle-down"),
+      #                             labels = ~ stringr::str_wrap(.x, width = 15)) +
+      #           # geom_text(aes(label = if_else(is.na(number),
+      #           #                               "NA", "")), # "value" shown on graph is "NA" for NAs, no text for HBs with values
+      #           #           na.rm = FALSE,
+      #           #           nudge_y = 2,
+      #           #           size = 4) +
+      #          theme_classic() +
+      #          theme(panel.grid.major.x = element_line(),
+      #                panel.grid.major.y = element_line(),
+      #                axis.title.x = element_text(size = 12,
+      #                                            color = "black",
+      #                                            face = "bold"),
+      #                axis.text.x = element_text(angle = 25)) + 
+      #          labs(x = paste0("Calendar Quarter"),
+      #               y = "Number of Patients") +
+      #          scale_y_continuous(#na.value = 0,   # for placing "NA" but this isn't working nicely (brings graph line down to wherever you place it)
+      #                             n.breaks = 10,
+      #                             expand = c(0,0)) +  # remove spacing between dates on x axis and 0  on y axis
+      #          coord_cartesian(ylim = c(0, (max(S2_plot3_data_for_graph()$number) + 
+      #                                          (0.1*max(S2_plot3_data_for_graph()$number)))), 
+      #                          expand = TRUE)
+      #         # ylim = c(0, (max(S2_plot3_data_for_graph()$number) + (0.1*max(S2_plot3_data_for_graph()$number)))) # having issues with NAs not allowing axis to extend 
+      #    })
+      #    
+      #    ### Run graph 3 through plotly
+      #    ggplotly(S2_plot3_graph(),
+      #             tooltip = "text") %>%     # uses text set up in ggplot aes above.
+      #     #  layout(legend = list(orientation = 'h')) %>%   # After discussion, we don't want this - moves legend to bottom
+      #       ### Remove unnecessary buttons from the modebar
+      #    config(displayModeBar = TRUE,
+      #           modeBarButtonsToRemove = bttn_remove,
+      #           displaylogo = F, editable = F)
+      #    
+      # })
+
+
+   ## Table for graph 3 ----
+   
+   
+   output$S2_3_table <- renderDataTable({
+      datatable(
+         S2_plot3_data_for_table() %>% 
+            # Add "NA" as a value to table on dashboard: 
+            mutate(across(number_of_patients_followed_up:percentage_followed_up, ~replace(., is.na(.), "NA"))),, 
+         style = 'bootstrap', 
+         class = 'table_bordered table-condensed',
+         rownames = FALSE, 
+         options = list(
+            pageLength = 16, 
+            autoWidth = FALSE, 
+            dom = 'tip'
+         ), 
+         colnames = c("Health Board", 
+                      "Calendar Quarter",
+                      "Number of Patients Followed Up", 
+                      "Total Number of Discharged Patients", 
+                      "Percentage (%) Followed Up")
+      )
    })
-   
-   ### Run graph 3 through plotly ----
-   ggplotly(S2_plot3_graph(),
-            tooltip = "text") %>%     # uses text set up in ggplot aes above.
-    #  layout(legend = list(orientation = 'h')) %>%   # After discussion, we don't want this - moves legend to bottom
-      ### Remove unnecessary buttons from the modebar ----
-   config(displayModeBar = TRUE,
-          modeBarButtonsToRemove = bttn_remove,
-          displaylogo = F, editable = F)
-   
-})
-
-
-## Table for graph 3 ----
-
-
-output$S2_3_table <- renderDataTable({
-   datatable(
-      S2_plot3_data_for_table() %>% 
-         # Add "NA" as a value to table on dashboard: 
-         mutate(across(number_of_patients_followed_up:percentage_followed_up, ~replace(., is.na(.), "NA"))),, 
-      style = 'bootstrap', 
-      class = 'table_bordered table-condensed',
-      rownames = FALSE, 
-      options = list(
-         pageLength = 16, 
-         autoWidth = FALSE, 
-         dom = 'tip'
-      ), 
-      colnames = c("Health Board", 
-                   "Calendar Quarter",
-                   "Number of Patients Followed Up", 
-                   "Total Number of Discharged Patients", 
-                   "Percentage (%) Followed Up")
-   )
-})
 
 
 ## Graph 3 data download button ----
