9036034447cd6d61ad893f8c590ed06c87082913
Mahri <mahri.stewart@phs.scot>
2025-04-23 12:18:43 +0000

    E1 - Graph 1 reminder title if user selects Orkney or Shetland, and NAs added to tables under graphs.

--- modules/indicators/E1_server.R
+++ modules/indicators/E1_server.R
@@ -65,97 +65,96 @@ output$E1_graph1_selected_areaName <- renderText({
 
 ## Create the discharges line chart ----
 
-   ### Render plotly ----
-
 output$E1_plot1 <- renderPlotly({
    
-   plot_ly(data = E1_plot1_Data(), 
-           
-           x = ~fyear, 
-           y = ~dd_bed_days, 
-           color = ~area_name, 
-           
-           # Tooltip text - Health board of treatment OR Council area of residence
-           text = if_else(E1_plot1_Data()$area_type == "Health board", 
-                          paste0("Financial year: ",
-                                 E1_plot1_Data()$fyear,
-                                 "<br>",
-                                 "Health board of treatment: ",
-                                 E1_plot1_Data()$area_name,
-                                 "<br>",
-                                 "Total number of days: ",
-                                 E1_plot1_Data()$dd_bed_days, 
-                                 "<br>",
-                                 "Rate per 1,000 population: ",
-                                 E1_plot1_Data()$rate_per_1000_population),
-                          paste0("Financial year: ",
-                                 E1_plot1_Data()$fyear,
-                                 "<br>",
-                                 "Council area of residence: ",
-                                 E1_plot1_Data()$area_name,
-                                 "<br>",
-                                 "Total number of days: ",
-                                 E1_plot1_Data()$dd_bed_days, 
-                                 "<br>",
-                                 "Rate per 1,000 population: ",
-                                 E1_plot1_Data()$rate_per_1000_population)),
-           hoverinfo = "text",
-           
-           # Line aesthetics: 
-           type = 'scatter',
-           mode = 'lines+markers', 
-           line = list(width = 3), 
-           colors = c("#3F3685", "#9B4393", "#0078D4", "#1E7F84"),
-           linetype = ~area_name, 
-           linetypes = c("solid", "dashed", "solid", "dashed"), 
-           symbol = ~area_name,
-           symbols = c("circle", "square", "triangle-up", "triangle-down"),
-           marker = list(size = 12),
-           # Size of graph:
-           height = 600,
-           # Legend info:
-           name = ~str_wrap(area_name, 15)) %>%
+   # Assign to an object so we can add Orkney/Shetland data title note afterwards
+   E1_plot1_plotly <- plot_ly(data = E1_plot1_Data(), 
+                    
+                    x = ~fyear, 
+                    y = ~dd_bed_days, 
+                    color = ~area_name, 
+                    
+                    # Tooltip text - Health board of treatment OR Council area of residence
+                    text = if_else(E1_plot1_Data()$area_type == "Health board", 
+                                   paste0("Financial year: ",
+                                          E1_plot1_Data()$fyear,
+                                          "<br>",
+                                          "Health board of treatment: ",
+                                          E1_plot1_Data()$area_name,
+                                          "<br>",
+                                          "Total number of days: ",
+                                          E1_plot1_Data()$dd_bed_days, 
+                                          "<br>",
+                                          "Rate per 1,000 population: ",
+                                          E1_plot1_Data()$rate_per_1000_population),
+                                   paste0("Financial year: ",
+                                          E1_plot1_Data()$fyear,
+                                          "<br>",
+                                          "Council area of residence: ",
+                                          E1_plot1_Data()$area_name,
+                                          "<br>",
+                                          "Total number of days: ",
+                                          E1_plot1_Data()$dd_bed_days, 
+                                          "<br>",
+                                          "Rate per 1,000 population: ",
+                                          E1_plot1_Data()$rate_per_1000_population)),
+                    hoverinfo = "text",
+                    
+                    # Line aesthetics: 
+                    type = 'scatter',
+                    mode = 'lines+markers', 
+                    line = list(width = 3), 
+                    colors = c("#3F3685", "#9B4393", "#0078D4", "#1E7F84"),
+                    linetype = ~area_name, 
+                    linetypes = c("solid", "dashed", "solid", "dashed"), 
+                    symbol = ~area_name,
+                    symbols = c("circle", "square", "triangle-up", "triangle-down"),
+                    marker = list(size = 12),
+                    # Size of graph:
+                    height = 600,
+                    # Legend info:
+                    name = ~str_wrap(area_name, 15)) %>%
+               
+               layout(# graph title is in a box above the graph and Orkney/Shetland 
+                      # reminder title is below this code. 
+                      yaxis = list(exponentformat = "none",
+                                   separatethousands = TRUE,  
+                                   range = c(0, max(E1_plot1_Data()$dd_bed_days) * 110 / 100), 
+                                   
+                                   # Wrap the y axis title in spaces so it doesn't cover the tick labels.
+                                   title = paste0(c(rep("&nbsp;", 20),
+                                                    print("Total Number of Days"), 
+                                                    rep("&nbsp;", 20),
+                                                    rep("\n&nbsp;", 3)),
+                                                  collapse = ""),#),
+                                   showline = TRUE, 
+                                   ticks = "outside"),
+                      
+                      xaxis = list(tickangle = -45,            # Diagonal x-axis ticks
+                                   title = paste0(c(rep("&nbsp;", 20),
+                                                    "<br>",
+                                                    "<br>",
+                                                    "Financial Year",
+                                                    rep("&nbsp;", 20),
+                                                    rep("\n&nbsp;", 3)),
+                                                  collapse = ""),
+                                   showline = TRUE, 
+                                   ticks = "outside"),
+                      
+                      # Set the graph margins:
+                      margin = list(l = 90, r = 60, b = 170, t = 90),  
+                      
+                      # Set the font sizes:
+                      font = list(size = 13),
+                      
+                      # Add a legend & make the legend background and border white:            
+                      showlegend = TRUE,
+                      legend = list(x = 1, 
+                                    y = 0.8, 
+                                    bgcolor = 'rgba(255, 255, 255, 0)', 
+                                    bordercolor = 'rgba(255, 255, 255, 0)'))%>%
       
-      layout(yaxis = list(exponentformat = "none",
-                          separatethousands = TRUE,  
-                          range = c(0, max(E1_plot1_Data()$dd_bed_days) * 110 / 100), 
-                          
-                          
-                          # Wrap the y axis title in spaces so it doesn't cover the tick labels.
-                          title = paste0(c(rep("&nbsp;", 20),
-                                           print("Total Number of Days"), 
-                                           rep("&nbsp;", 20),
-                                           rep("\n&nbsp;", 3)),
-                                         collapse = ""),#),
-                          showline = TRUE, 
-                          ticks = "outside"
-      ),
-      
-      xaxis = list(tickangle = -45,                    # Diagonal x-axis ticks
-                   title = paste0(c(rep("&nbsp;", 20),
-                                    "<br>",
-                                    "<br>",
-                                    "Financial Year",
-                                    rep("&nbsp;", 20),
-                                    rep("\n&nbsp;", 3)),
-                                  collapse = ""),
-                   showline = TRUE, 
-                   ticks = "outside"),
-      
-      # Set the graph margins:
-      margin = list(l = 90, r = 60, b = 170, t = 90),  
-      
-      # Set the font sizes:
-      font = list(size = 13),
-      
-      # Add a legend & make the legend background and border white:            
-      showlegend = TRUE,
-      legend = list(x = 1, 
-                    y = 0.8, 
-                    bgcolor = 'rgba(255, 255, 255, 0)', 
-                    bordercolor = 'rgba(255, 255, 255, 0)')) %>%
-      
-      # Remove any buttons we don't need from the modebar.
+      ### Remove any buttons we don't need from the modebar ----
       config(displayModeBar = TRUE,
              modeBarButtonsToRemove = list('select2d', 'lasso2d', 
                                            # 'zoomIn2d', 'zoomOut2d', 'autoScale2d', 
@@ -164,15 +163,38 @@ output$E1_plot1 <- renderPlotly({
                                            'hoverClosestCartesian'), 
              displaylogo = F, 
              editable = F)
-   
-   
+  
+     ### Add Orkney/ Shetland reminder title ---- 
+  
+         if #("NHS Orkney" %in% input$E1_plot1_areaName &    # Not necessary
+        #     "NHS Shetland" %in% input$E1_plot1_areaName) {
+        #    E1_plot1_plotly <- E1_plot1_plotly %>% 
+        #       layout(title = "NHS Orkney & NHS Shetland values are included in NHS Grampian data")
+        #    } else if 
+             ("NHS Orkney" %in% input$E1_plot1_areaName) {
+              E1_plot1_plotly <- E1_plot1_plotly %>%
+                 layout(title = "NHS Orkney & NHS Shetland values are included in NHS Grampian data")
+              } else if ("NHS Shetland" %in% input$E1_plot1_areaName) {
+                 E1_plot1_plotly <- E1_plot1_plotly %>% 
+                    layout(title = "NHS Orkney & NHS Shetland values are included in NHS Grampian data")
+                 } else {
+                    E1_plot1_plotly <- E1_plot1_plotly %>% layout(title = NULL)
+                    }
+  
+  
+  ### Return the plot ----
+  E1_plot1_plotly
+  
+      
 })
 
 
 ## Table below graph 1 ----
  
  output$E1_1_table <- renderDataTable({
-   datatable(E1_plot1_Data(),
+   datatable(E1_plot1_Data() %>% 
+                # Add "NA" as a value to table on dashboard:
+                mutate(across(dd_bed_days:rate_per_1000_population, ~replace(., is.na(.), "NA"))),
              style = 'bootstrap',
              class = 'table-bordered table-condensed',
              rownames = FALSE,
@@ -216,17 +238,28 @@ output$E1_plot2_year_output <- renderUI({
 })
 
 ## Selecting appropriate data for graph 2 ---- 
-
+# Taking Orkney and Shetland out of the graph, adding ordering, adding colours for graph
 E1_plot2_Data <- reactive({
    E1_data %>%
       select(fyear, area_type, area_name, dd_bed_days, rate_per_1000_population) %>%
-      filter(area_type == "Health board") %>% 
+     # filter(area_type == "Health board") %>% 
+      filter(area_type == "Health board" &
+                (area_type == "Health board" &
+                    (area_name != "NHS Orkney" & area_name !="NHS Shetland"))) %>%
       filter(fyear %in% input$E1_plot2_year) %>% 
       mutate(area_name = fct_reorder(area_name, rate_per_1000_population)) %>%    # for ordering by most to least bed days
       mutate(to_highlight = if_else(area_name == "NHS Scotland",                  # for highlighting NHS Scotland 
                                     "#3F3685", "#0078D4"))                       # changed from "yes" and "no" so that these colours 
 })                                                             # appear on the graph and don't have a legend using "marker = list(color... )"
 
+## Selecting appropriate data for table 2 ---- 
+E1_plot2_Data_for_table <- reactive({
+   E1_data %>%
+      select(fyear, area_type, area_name, dd_bed_days, rate_per_1000_population) %>%
+      filter(area_type == "Health board")  %>%
+      filter(fyear %in% input$E1_plot2_year) 
+   })
+   
 ## Reactive graph 2 title ---- 
 
 output$E1_graph2_selected_fyear <- renderUI({
@@ -298,8 +331,9 @@ output$E1_plot2 <- renderPlotly({
 ## Graph 2 data table ----
 
 output$E1_2_table <- renderDataTable({
-  datatable(E1_plot2_Data() %>% 
-               select(!c("to_highlight")),
+  datatable(E1_plot2_Data_for_table() %>% 
+               # Add "NA" as a value to table on dashboard:
+               mutate(across(dd_bed_days:rate_per_1000_population, ~replace(., is.na(.), "NA"))),
             style = 'bootstrap',
             class = 'table-bordered table-condensed',
             rownames = FALSE,
@@ -315,12 +349,10 @@ output$E1_2_table <- renderDataTable({
 ## Table 2 download button ---- 
 # allows user to download selected data in .csv format
 
-
 output$E1_2_table_download <- downloadHandler(
   filename = 'E1 - Bed days per 1000 population when patient ready for discharge for chosen year.csv',
   content = function(file) {
-    write.table(E1_plot2_Data()%>% 
-                   select(!"to_highlight"),
+    write.table(E1_plot2_Data_for_table(),
                 file,
                 #Remove row numbers as the .csv file already has row numbers.
                 row.names = FALSE,
