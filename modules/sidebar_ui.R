394385243beb51da64dfc743ad74daa6ea5f2526
laurem06 <lauren.macarthur@phs.scot>
2026-01-14 21:03:22 +0000

    sidebar updates removing and adding updated flag

--- modules/sidebar_ui.R
+++ modules/sidebar_ui.R
@@ -19,17 +19,15 @@ sidebarMenu(
   br(),
   ## Timely Tabs ----          
   menuItem("Timely :", icon = icon("clock")),
-  menuItem("T1 - Adult", tabName = "T1_tab", icon = icon("link"),
+  menuItem("T1 - Adult", tabName = "T1_tab", icon = icon("link")),
+  menuItem("T2 - Child and Adolescent", tabName = "T2_tab", icon = icon("up-right-from-square")),
+  menuItem("T3 - Drugs or Alcohol", tabName = "T3_tab", icon = icon("up-right-from-square"),
            badgeLabel = "Updated", badgeColor = "orange"),
-  menuItem("T2 - Child and Adolescent", tabName = "T2_tab", icon = icon("up-right-from-square"),
-           badgeLabel = "Updated", badgeColor = "orange"),
-  menuItem("T3 - Drugs or Alcohol", tabName = "T3_tab", icon = icon("up-right-from-square")),
         
   br(),
   ## Safe Tabs ----           
   menuItem("Safe :"),
-  menuItem("S1 - Suicide", tabName = "S1_tab", icon = icon("image"),
-           badgeLabel = "Updated", badgeColor = "orange"),
+  menuItem("S1 - Suicide", tabName = "S1_tab", icon = icon("image")),
   menuItem("S2 - Community Follow-up", tabName = "S2_tab", icon = icon("bar-chart"), 
            badgeLabel = "Updated", badgeColor = "orange"),
   menuItem("S5 - Physical Violence", tabName = "S5_tab", icon = icon("bar-chart"), 
@@ -41,12 +39,14 @@ sidebarMenu(
   menuItem("P1 - Carers", tabName = "P1_tab", icon = icon("image")),
   menuItem("P2 - At Home Support", tabName = "P2_tab", icon = icon("image")),
   menuItem("P3 - Listened To", tabName = "P3_tab", icon = icon("image")),
-  menuItem("P4 - Advance Statements", tabName = "P4_tab", icon = icon("image")),
+  menuItem("P4 - Advance Statements", tabName = "P4_tab", icon = icon("image"),
+           badgeLabel = "Updated", badgeColor = "orange"),
             
   br(),
   ## Effective Tabs ----           
   menuItem("Effective :"),
-  menuItem("E1 - Delayed Discharge", tabName = "E1_tab", icon = icon("bar-chart")),
+  menuItem("E1 - Delayed Discharge", tabName = "E1_tab", icon = icon("bar-chart"),
+           badgeLabel = "Updated", badgeColor = "orange"),
           
   br(),
   ## Efficient Tabs ----           
@@ -55,8 +55,7 @@ sidebarMenu(
            badgeLabel = "Updated", badgeColor = "orange"),
   menuItem("EF 2 - Readmissions", tabName = "EF2_tab", icon = icon("image"), 
            badgeLabel = "Updated", badgeColor = "orange"),
-  menuItem("EF 3 - Psychiatric Beds", tabName = "EF3_tab", icon = icon("image"),
-           badgeLabel = "Updated", badgeColor = "orange"),
+  menuItem("EF 3 - Psychiatric Beds", tabName = "EF3_tab", icon = icon("image")),
   menuItem("EF 4 - Mental Health Spend", tabName = "EF4_tab", icon = icon("bar-chart")),
   menuItem("EF 5 - Community DNA", tabName = "EF5_tab", icon = icon("bar-chart"), 
            badgeLabel = "Updated", badgeColor = "orange"),
@@ -65,7 +64,8 @@ sidebarMenu(
   ## Equitable Tabs ----           
   menuItem("Equitable :"),
   menuItem("EQ 1 - Premature Mortality", tabName = "EQ1_tab", icon = icon("bar-chart")),
-  menuItem("EQ 2 - Emergency Detention", tabName = "EQ2_tab", icon = icon("image")),
+  menuItem("EQ 2 - Emergency Detention", tabName = "EQ2_tab", icon = icon("image"),
+           badgeLabel = "Updated", badgeColor = "orange"),
   menuItem("EQ 4 - U18 Outwith CAMH Wards", tabName = "EQ4_tab", icon = icon("image"), 
            badgeLabel = "Updated", badgeColor = "orange")
             
