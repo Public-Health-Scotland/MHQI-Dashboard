45902c1e85ffba8bdee2fcad3928c9a8c3d548bb
Alex Bruce <74981565+alex-bruce@users.noreply.github.com>
2026-05-22 18:44:15 +0100

    Apply suggestion from @alex-bruce
    Clarifying type of bed day

--- modules/indicators/EF1_ui.R
+++ modules/indicators/EF1_ui.R
@@ -15,7 +15,7 @@ tabItem(tabName = "EF1_tab",
           
           # Title for EF1 tab ----
           
-          h1("EF1 - Rate of emergency bed days for adults (per 100,000 population)"),
+          h1("EF1 - Rate of emergency psychiatric bed days for adults (per 100,000 population)"),
           h3("Last Updated: March 2026"),
           
           hr(),       # page break
