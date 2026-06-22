3c0d3e45b57053368e1e9717ca22d335b51ac40f
laurem06 <lauren.macarthur@phs.scot>
2026-06-22 21:39:39 +0000

    P1, P2 and P3: - last updated and next updated date - add note about mental health condition changing

--- modules/indicators/P2_ui.R
+++ modules/indicators/P2_ui.R
@@ -2,7 +2,7 @@ tabItem(tabName = "P2_tab",
         fluidPage(
           
           h1("P2 - % of adults with mental health problems supported at home who agree that their services and support had an impact in improving or maintaining their quality of life"),
-          h3("Last Updated: September 2024"),
+          h3("Last Updated: June 2026"),
           fluidRow(
             box(width = 9,
                 img(src='infographics/P2.png',
@@ -13,10 +13,12 @@ tabItem(tabName = "P2_tab",
           fluidRow(
             box(width = 9,
                 h2("Data source information and notes:"),
-                p("Due to a change in the source of the indicator in 2024, the data is 
-                not comparable to previous data for 2021/22. The data is presented 
-                as a range due to it being drawn from a survey on a sample and 
-                not population data. Further information can be found in the ",
+                p("*The filter question used to identify individuals with a mental health
+                  condition in the  Health and Care Experience (HACE) Survey changed between 2023/24 and 2025/26. In 2023/24, respondents were
+                  asked if they had a condition lasting 12 months with 'mental health condition' being one of the options
+                  to tick. In 2025/26, respondents were asked if they had any conditions lasting 12 months
+                  or more. Only those that answered yes to this question were then asked about areas this affected
+                  with 'mental health' being one of the options to tick. Further information can be found in the",
                   a(href="https://www.gov.scot/collections/health-and-care-experience-survey/",
                     target = "_blank",
                     "Health and Care Experience Survey.")), 
@@ -30,7 +32,7 @@ tabItem(tabName = "P2_tab",
                 p("- Aspects of care and support provided by local authorities 
                   and other organisations."), 
                 p("- Caring responsibilities and related support."),
-                p("Next update: July 2026")
+                p("Next update: July 2028")
             )
           ),
           
