e5c9a958aa2707024b0f71708dde22e09b67b457
alex-bruce <alex.bruce@phs.scot>
2026-09-18 18:07:17 +0000

    Adding new ui structure for indicators to app.R

--- app.R
+++ app.R
@@ -39,129 +39,283 @@ list.files("functions") %>%
 #* Read in credentials for password-protecting the app ----
 # credentials <- readRDS("admin/release_credentials.rds") # Un-comment if password protection needed
 
+# # Get packages
+# source("setup.R")
+# 
+# # Getting UI for modules
+# source(file.path("modules/summary_button/summary_button_ui.R"), local = TRUE)$value
+source(file.path("modules/alt_text/alt_text_modals_ui.R"), local = TRUE)$value
+# source(file.path("modules/definitions/definitions_ui.R"), local = TRUE)$value
 
-### [ UI section ] -------------------------------------------------------------
-
-ui <- 
-  # secure_app( # Un-comment if password protection is needed.
-  dashboardPage(
-  
-  dashboardHeader(title = "MH Quality Indicators"),
-  
-  dashboardSidebar(source("modules/sidebar_ui.R", local = TRUE)$value),
-  
-  # Define how the various pages of the dashboard look
-  dashboardBody(
-    
-    ## Source styling for shinydashboard elements 
-    # - must be before stylesheet so that it doesn't override the css file
-    source("www/dashboard_style.R", local = TRUE)$value,
-    
-    ## load css stylesheet that defines how things look 
-    tags$head(includeCSS("www/stylesheet.css")),
-    
-    ## Tabs ----
-    tabItems(
-      # [Introduction Tab] ----
-      source("modules/introduction_ui.R", local = TRUE)$value,
+# UI ----
+ui <- fluidPage(
+  tagList(
+    # For go to top chevrons on scroll down
+    use_gotop(),
+    # Specify most recent fontawesome library - change version as needed
+    navbarPage(
+      id = "intabset", # id used for jumping between tabs
+      position = "fixed-top",
+      collapsible = "true",
+      # Specify language for accessibility
+      #lang = "en",
+      #tags$html(lang="en"),
+      title = div(
+        tags$a(img(src = "white-logo.png", height = 40,
+                   alt ="Go to Public Health Scotland (external site)"),
+               href = "https://www.publichealthscotland.scot/",
+               target = "_blank"), # PHS logo links to PHS website
+        style = "position: relative; top: -10px;"),
+      windowTitle = "Mental Health Quality Indicators - Public Health Scotland",# Title for browser tab
+      header = source(file.path("header.R"), local=TRUE)$value,
       
-      # [Scotland Hub Tab] ----
-      source("modules/scot_hub_ui.R", local = TRUE)$value,
+      ##############################################.
+      # INTRO PAGE ----
+      ##############################################.
+      tabPanel(title = "Introduction",
+               icon = icon_no_warning_fn("circle-info"),
+               value = "intro",
+
+               source(file.path("indicators/introduction/introduction_ui.R"), local = TRUE)$value
+
+      ), # tabpanel
       
-      # [Glossary Tab] ----
-      source("modules/glossary_ui.R", local = TRUE)$value,
+      ##############################################.
+      # Scot Hub ----
+      ##############################################.
+      tabPanel(title = "Scotland Hub",
+               icon = icon_no_warning_fn("square-poll-vertical"),
+               value = "scot_hub",
+
+               source("modules/scot_hub_ui.R", local = TRUE)$value
+
+      ), # tabpanel
       
-      # [Data Downloads Tab] ----
-      source("modules/data_ui.R", local = TRUE)$value,
       
-      ## Sourcing ui sections for each indicator ----
-      # [Timely] ----
-      source("modules/indicators/T1_ui.R", local = TRUE)$value,
-      source("modules/indicators/T2_ui.R", local = TRUE)$value,
-      source("modules/indicators/T3_ui.R", local = TRUE)$value,
+      ##############################################.
+      ## Safe ----
+      ##############################################.
+      tabPanel(title ="Safe",
+               # Look at https://fontawesome.com/search?m=free for icons
+               icon = icon_no_warning_fn("clipboard-check"),
+               value = "safe",
+               navlistPanel(widths = c(2,10), id = "safe_panel", #icon = icon_no_warning_fn("spa")
+                            
+                            tabPanel(title = "S1 - Suicide Rates",
+                                     value = "S1_suicide_rates",
+                                     column(12, source(file.path("indicators/S1_suicide_rates/S1_ui.R"), local = TRUE)$value)),
+                            
+                            tabPanel(title = "S5 - Physical Violence in Psychiatric Beds",
+                                     value = "S5_psych_physical_violence",
+                                     column(12, source(file.path("indicators/S5_psych_physical_violence/S5_ui.R"), local = TRUE)$value)),
+                            
+                            # tabPanel(title = "EQ1 - Premature Mortality",
+                            #          value = "EQ1_premature_mortality",
+                            #          column(12, source(file.path("indicators/EQ1_premature_mortality/EQ1_ui.R"), local = TRUE)$value)),
+                            
+                            tabPanel(title = "EQ4 - U18s Admitted Outwith CAMH Facilities",
+                                     value = "EQ4_u18_outwith_camh",
+                                     column(12, source(file.path("indicators/EQ4_u18_outwith_camh/EQ4_ui.R"), local = TRUE)$value))
+               ) # navbarlistPanel
+      ), # tabPanel
       
-      # [Safe] ----
-      source("modules/indicators/S1_ui.R", local = TRUE)$value,
-      source("modules/indicators/S2_ui.R", local = TRUE)$value,
-      source("modules/indicators/S5_ui.R", local = TRUE)$value,
+      
+      ##############################################.
+      ## Effective ----
+      ##############################################.
+      tabPanel(title ="Effective",
+               # Look at https://fontawesome.com/search?m=free for icons
+               icon = icon_no_warning_fn("clipboard-check"),
+               value = "effective",
+               navlistPanel(widths = c(2,10), id = "effective_panel", #icon = icon_no_warning_fn("spa")
+                            
+                            tabPanel(title = "EF1 - Emergency Bed Days",
+                                     value = "EF1_emergency_bed_days",
+                                     column(12, source(file.path("indicators/EF1_emergency_bed_days/EF1_ui.R"), local = TRUE)$value)),
+                            
+                            tabPanel(title = "EF2 - Readmissions",
+                                     value = "EF2_readmissions",
+                                     column(12, source(file.path("indicators/EF2_readmissions/EF2_ui.R"), local = TRUE)$value))
+               ) # navbarlistPanel
+      ), # tabPanel
+      
+      
+      ##############################################.
+      ## Timely ----
+      ##############################################.
+      tabPanel(title ="Timely",
+               # Look at https://fontawesome.com/search?m=free for icons
+               icon = icon_no_warning_fn("clipboard-check"),
+               value = "timely",
+               navlistPanel(widths = c(2,10), id = "timely_panel", #icon = icon_no_warning_fn("spa")
+                            
+                            tabPanel(title = "T1 - Adult Psychiatric Referral",
+                                     value = "T1_adult_psych_referral",
+                                     column(12, source(file.path("indicators/T1_adult_psych_referral/T1_ui.R"), local = TRUE)$value)),
+               
+                            tabPanel(title = "T2 - U18 Psychaitric Referral",
+                                     value = "T2_u18_psych_referral",
+                                     column(12, source(file.path("indicators/T2_u18_psych_referral/T2_ui.R"), local = TRUE)$value)),
+                            
+                            tabPanel(title = "T3 - Drugs & Alcohol Referral",
+                                     value = "T3_drugs_alcohol_referral",
+                                     column(12, source(file.path("indicators/T3_drugs_alcohol_referral/T3_ui.R"), local = TRUE)$value))
+               ) # navbarlistPanel
+      ), # tabPanel
+      
+      
+      ##############################################.
+      ## Efficient ----
+      ##############################################.
+      tabPanel(title ="Efficient",
+               # Look at https://fontawesome.com/search?m=free for icons
+               icon = icon_no_warning_fn("clipboard-check"),
+               value = "efficient",
+               navlistPanel(widths = c(2,10), id = "efficient_panel", #icon = icon_no_warning_fn("spa")
+                            
+                            tabPanel(title = "E1 - Delayed Discharge Days",
+                                     value = "E1_delayed_discharge",
+                                     column(12, source(file.path("indicators/E1_delayed_discharge/E1_ui.R"), local = TRUE)$value)),
+                            
+                            # tabPanel(title = "EF3 - Psychiatric Bed Days",
+                            #          value = "EF3_psychiatric_beds",
+                            #          column(12, source(file.path("indicators/EF3_psychiatric_beds/EF3_ui.R"), local = TRUE)$value)),
+                            
+                            tabPanel(title = "EF4 - Mental Health Spend",
+                                     value = "EF4_mental_health_spend",
+                                     column(12, source(file.path("indicators/EF4_mental_health_spend/EF4_ui.R"), local = TRUE)$value)),
+                            
+                            tabPanel(title = "EF5 - Community DNAs",
+                                     value = "EF5_community_dna",
+                                     column(12, source(file.path("indicators/EF5_community_dna/EF5_ui.R"), local = TRUE)$value)),
+                            
+                            tabPanel(title = "EQ2 - Emergency Detention",
+                                     value = "EQ2_emergency_detention",
+                                     column(12, source(file.path("indicators/EQ2_emergency_detention/EQ2_ui.R"), local = TRUE)$value))
+               ) # navbarlistPanel
+      ), # tabPanel
+      
+      
+      ##############################################.
+      ## Integrated ----
+      ##############################################.
+      tabPanel(title ="Integrated",
+               # Look at https://fontawesome.com/search?m=free for icons
+               icon = icon_no_warning_fn("clipboard-check"),
+               value = "integrated",
+               navlistPanel(widths = c(2,10), id = "integrated_panel", #icon = icon_no_warning_fn("spa")
+                            
+                            tabPanel(title = "S2 - Psychiatric Discharge Follow Up %",
+                                     value = "S2_psych_community_followup",
+                                     column(12, source(file.path("indicators/S2_psych_community_followup/S2_ui.R"), local = TRUE)$value))
+               ) # navbarlistPanel
+      ), # tabPanel
+      
+      
+      ##############################################.
+      ## Person Centred ----
+      ##############################################.
+      tabPanel(title ="Person Centred",
+               # Look at https://fontawesome.com/search?m=free for icons
+               icon = icon_no_warning_fn("clipboard-check"),
+               value = "person_centred",
+               navlistPanel(widths = c(2,10), id = "person_centred_panel", #icon = icon_no_warning_fn("spa")
+                            
+                            tabPanel(title = "E1 - Delayed Discharge Days",
+                                     value = "E1_delayed_discharge",
+                                     column(12, source(file.path("indicators/E1_delayed_discharge/E1_ui.R"), local = TRUE)$value)),
 
-      # [Person Centred] ----
-      source("modules/indicators/P1_ui.R", local = TRUE)$value,
-      source("modules/indicators/P2_ui.R", local = TRUE)$value,
-      source("modules/indicators/P3_ui.R", local = TRUE)$value,
-      source("modules/indicators/P4_ui.R", local = TRUE)$value,
-
-      # [Effective] ----
-      source("modules/indicators/E1_ui.R", local = TRUE)$value,
-
-      # [Efficient] ----
-      source("modules/indicators/EF1_ui.R", local = TRUE)$value,
-      source("modules/indicators/EF2_ui.R", local = TRUE)$value,
-      source("modules/indicators/EF3_ui.R", local = TRUE)$value,
-      source("modules/indicators/EF4_ui.R", local = TRUE)$value,
-      source("modules/indicators/EF5_ui.R", local = TRUE)$value,
-
-      # [Equitable] ----
-      source("modules/indicators/EQ1_ui.R", local = TRUE)$value,
-      source("modules/indicators/EQ2_ui.R", local = TRUE)$value,
-      source("modules/indicators/EQ4_ui.R", local = TRUE)$value,
-
-      # [Appendix tab] ----
-      source("modules/appendix_ui.R", local = TRUE)$value
-    
-   ) # End of tabItems
-  ) # End of dashboardBody
-) # End of dashboardPage
-# ) # End of password-protection wrapper
+                            tabPanel(title = "EF4",
+                                     value = "EF4_mental_health_spend",
+                                     column(12, source(file.path("indicators/EF4_mental_health_spend/EF4_ui.R"), local = TRUE)$value)),
+                            
+                            tabPanel(title = "EF5",
+                                     value = "EF5_community_dna",
+                                     column(12, source(file.path("indicators/EF5_community_dna/EF5_ui.R"), local = TRUE)$value)),
+                            
+                            tabPanel(title = "EQ2",
+                                     value = "EQ2_emergency_detention",
+                                     column(12, source(file.path("indicators/EQ2_emergency_detention/EQ2_ui.R"), local = TRUE)$value))
+               ) # navbarlistPanel
+      ), # tabPanel
 
 
+      ##############################################.
+      # METADATA ----
+      ##############################################.
+      tabPanel(title = "Metadata",
+               # Look at https://fontawesome.com/search?m=free for icons
+               icon = icon_no_warning_fn("file-pen"),
+               value = "metadata",
 
-### [ Server ] -----------------------------------------------------------------
+               source(file.path("indicators/metadata/metadata_ui.R"), local = TRUE)$value
+
+      ), # tabpanel
+      ##############################################.
+      # DATA DOWNLOAD ----
+      ##############################################.
+      tabPanel(title = "Open data",
+               # Look at https://fontawesome.com/search?m=free for icons
+               icon = icon_no_warning_fn("floppy-disk"),
+               value = "download",
+
+               source(file.path("indicators/download/download_ui.R"), local = TRUE)$value
+
+      ), # tabpanel
+    ) # navbar
+  ) # taglist
+) # ui fluidpage
+
+# ----------------------------------------------
+# Server
+
 server <- function(input, output, session) {
-  
-  ##* Shinymanager authorisation ----
-  # Un-comment this section to password protect the app.
-  # Re-comment out to remove password protection on launch day.
-   # res_auth <- secure_server(
-   # check_credentials = check_credentials(credentials)
-   # )
-   # 
-   # output$auth_output <- renderPrint({
-   # reactiveValuesToList(res_auth)
-   # })
-  
-  # Navigation buttons ----
-  source("modules/nav_buttons_server.R", local = TRUE)
-  
-  # Scothub
-  source("modules/scot_hub_server.R", local = TRUE)
-  
-  # Data downloads ----
-  source("modules/data_server.R", local = TRUE)
-  
-  # Indicator servers
-  source("modules/indicators/E1_server.R", local = TRUE)
-  source("modules/indicators/EF2_server.R", local = TRUE)
-  source("modules/indicators/EQ1_server.R", local = TRUE)
-  source("modules/indicators/EQ4_server.R", local = TRUE)
-  source("modules/indicators/EF1_server.R", local = TRUE)
-  source("modules/indicators/EF4_server.R", local = TRUE)
-  source("modules/indicators/EF5_server.R", local = TRUE)
-  source("modules/indicators/S1_server.R", local = TRUE)
-  source("modules/indicators/S2_server.R", local = TRUE)
-  source("modules/indicators/S5_server.R", local = TRUE)
+
+  if(password_protect){
+    test <-  FALSE # set this to TRUE to deploy test version of the app
+    source(file.path("password_protect/password_protect_server.R"), local = TRUE)$value
+  }
+
+  # Get modules
+  source(file.path("modules/alt_text/alt_text_modals_server.R"), local = TRUE)$value
+  source(file.path("modules/summary_button/summary_button_server.R"), local = TRUE)$value
+
+
+  # Get functions
+  source(file.path("functions/core_functions.R"), local = TRUE)$value
+  source(file.path("functions/plot_functions.R"), local = TRUE)$valuee
+  # source(file.path("indicators/cases/cases_functions.R"), local = TRUE)$value
+  # source(file.path("indicators/hospital_admissions/hospital_admissions_functions.R"), local = TRUE)$value
+  # source(file.path("indicators/respiratory/respiratory_functions.R"), local = TRUE)$value
+  # source(file.path("indicators/respiratory_mem/respiratory_mem_functions.R"), local = TRUE)$value
+  # source(file.path("indicators/mortality/euromomo/euromomo_functions.R"), local = TRUE)$value
+  # source(file.path("indicators/wastewater/wastewater_functions.R"), local = TRUE)$value
+
+  # Get content for individual pages
+  source(file.path("indicators/introduction/introduction_server.R"), local = TRUE)$value
+  source(file.path("indicators/at_a_glance/at_a_glance_server.R"), local = TRUE)$value
   
   
-  # Keep dashboard active indefinitely to meet accessibility requirements
-  # (Keep at the end of server)
+  
+  source(file.path("indicators/download/download_server.R"), local = TRUE)$value
+  
+
   auto_invalidate <- reactiveTimer(10000)
   observe({
     auto_invalidate()
     cat(".")
   })
-}
 
-# Sets language right at the top of source (required this way for screen readers)
+
+}
+#sets language right at the top of source (required this way for screen readers)
 attr(ui, "lang") = "en"
 
-shinyApp(ui, server)
+#conditionally password protect app
+if (password_protect){ ui <- secure_app(ui) }
+
+
+# Run the application
+shinyApp(ui=ui, server=server)
+
+### END OF SCRIPT ###
