566ecba29799c51c2b2e7ca3e37558dc9ae7f9c9
laurem06 <lauren.macarthur@phs.scot>
2026-09-24 20:21:01 +0000

    EF2 ui: update last updated and next updated date. Update completeness info EF2 server: alter graph range and update quarter selected to apr-jun 2026

--- modules/indicators/EF2_server.R
+++ modules/indicators/EF2_server.R
@@ -99,7 +99,7 @@ output$EF2_trendPlot <- renderPlotly({
                    # quarter (i.e. it will be (-0.5, 12.5) for July 2025 update)
                    # Starting at -0.5 and ending at 11.5 gives much nicer 
                    # spacing on the axis than "0, 12"
-                   range = list(-0.5, 15.5),
+                   range = list(-0.5, 16.5),
                    showline = TRUE, 
                    ticks = "outside"),
       
@@ -181,7 +181,7 @@ output$EF2_plot2_quarter_output <- renderUI({
     "EF2_plot2_quarter",
     label = "Select calendar quarter:",
     choices = unique(EF2_data$year_months),
-    selected = "Jan-Mar 2026")
+    selected = "Apr-Jun 2026")
 })
 
 ## Selecting appropriate data for graph 2 ---- 
